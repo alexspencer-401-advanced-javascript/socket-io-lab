ca3d2ebe0472e7e775a4bf87c18991843df7fd74
Alex Spencer <alexander.j.spencer@gmail.com>
2019-10-09 16:31:19 -0700

    so i can push, tests not finished

--- __tests__/file-reader.test.js
+++ __tests__/file-reader.test.js
@@ -1,9 +1,26 @@
-const readFile = require('../file-reader');
+jest.mock('../files', () => {
+  return {
+    readFile: jest.fn(),
+    writeFile: jest.fn()
+  };
+});
 
-describe('file reader teste', () => {
-  it('writes a file', () => {
-    const text = 'this is an example string'
-    const newFile = readFile(text);
-    expect(newFile).toEqual({});
+const { readFile, writeFile } = require('../files')
+const fileReader = require('../file-reader');
+const fileWriter = require('../file-writer');
+
+describe('file reader test', () => {
+  it('reads a file', () => {
+    const source = './example.txt';
+    const fileContents = 'this is a fake file'
+    readFile.mockResolvedValue(fileContents);
+
+    return fileReader(source)
+      .then(contents => {
+        const readCalls = readFile.mock.calls;
+        expect(contents).toBe(fileContents);
+        expect(readCalls.length).toBe(1);
+        expect(readCalls[0][0]).toBe(source);
+      })
   })
 })
